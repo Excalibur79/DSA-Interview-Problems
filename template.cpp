b8742a8112cede917f325e83036a758eb188ad7d
Ankur Saha <ankursahamoto@gmail.com>
2021-11-13 20:41:00 +0530

    second commit

--- template.cpp
+++ template.cpp
@@ -20,24 +20,7 @@ const ll INF = 1e18;
 #define printarr(arr, n) \
     for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
 
-void solve() {
-    vi nums = {0, 1, 2, 1, 0, 1};
-    int numberOfOnes = 0, numberOfTwos = 0, numberOfZeroes = 0;
-    for (int value : nums) {
-        if (value == 0)
-            numberOfZeroes++;
-        else if (value == 1)
-            numberOfOnes++;
-        else
-            numberOfTwos++;
-    }
-    int k = 0;
-    while (numberOfZeroes--) nums[k++] = 0;
-    while (numberOfOnes--) nums[k++] = 1;
-    while (numberOfTwos--) nums[k++] = 2;
-
-    printarr(nums, nums.size());
-}
+void solve() {}
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
